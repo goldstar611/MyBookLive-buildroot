92d8ad6558185af9baa607fb3f3008fc2a516afe
goldstar611 <goldstar611@hotmail.com>
2017-05-28 09:03:40 -0500

    Adding initial minimal buildroot defconfig for mybooklive and linux patches from https://github.com/MyBookLive

---
+++ board/mybooklive/post-build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# post-build.sh for MyBookLive taken from CubieBoard's post-build.sh
+# 2013, Carlo Caione <carlo.caione@gmail.com>
+
+BOARD_DIR="$(dirname $0)"
+MKIMAGE=$HOST_DIR/usr/bin/mkimage
+BOOT_CMD=$BOARD_DIR/boot.cmd
+BOOT_CMD_H=$BINARIES_DIR/boot.scr
+
+# U-Boot script
+$MKIMAGE -C none -A powerpc -T script -a 0 -e 0 -n 'Execute uImage' -d $BOOT_CMD $BOOT_CMD_H
+# Copy U-Boot script to rootfs
+cp $BOOT_CMD_H $TARGET_DIR/boot/boot.scr
