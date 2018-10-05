cfb8beb0d229430b2374d77b55b90f208870ce9b
Your Name <you@example.com>
2018-10-05 04:43:58 -0500

    Copy U-Boot script only if /boot/ exists

--- board/mybooklive/post-build.sh
+++ board/mybooklive/post-build.sh
@@ -10,4 +10,6 @@ BOOT_CMD_H=$BINARIES_DIR/boot.scr
 # U-Boot script
 $MKIMAGE -C none -A powerpc -T script -a 0 -e 0 -n 'Execute uImage' -d $BOOT_CMD $BOOT_CMD_H
 # Copy U-Boot script to rootfs
-cp $BOOT_CMD_H $TARGET_DIR/boot/boot.scr
+if [ -d "$TARGET_DIR/boot" ]; then
+  cp $BOOT_CMD_H $TARGET_DIR/boot/boot.scr
+fi
